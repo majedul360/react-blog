1c132ced9bd4f4db2791c94939fbfe2bc7a7896e
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-03-21 21:18:04 +0600

    react router added done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import Header from "./components/Header/Header";
 import "./App.scss";
 import Home from "./pages/home/Home";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Registar from "../src/pages/registar/Registar";
+import Login from "../src/pages/login/Login";
+import Single from "../src/pages/single/Single";
+import Write from "../src/pages/write/Write";
+import Settings from "../src/pages/settings/Settings";
 function App() {
+  const currentUser = true;
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/posts">
+            <Home />
+          </Route>
+          <Route path="/registar">
+            {currentUser ? <Home /> : <Registar />}
+          </Route>
+          <Route path="/login">{currentUser ? <Home /> : <Login />}</Route>
+          <Route path="/post/:Id">
+            <Single />
+          </Route>
+          <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+          <Route path="/settings">
+            {currentUser ? <Settings /> : <Login />}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,29 +3,57 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import Socialmedia from "../socialMedia/SocialMedia";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isShowMenu, setIsShowMenu] = useState(false);
   const menuHandler = () => {
     setIsShowMenu(!isShowMenu);
   };
+  const user = true;
   return (
     <div className="header">
       <div className="left">
         <Socialmedia />
       </div>
       <nav className={`${isShowMenu && "active"}`}>
-        <span onClick={() => setIsShowMenu(false)}>Home</span>
+        <span onClick={() => setIsShowMenu(false)}>
+          <Link className="link" to="/">
+            Home
+          </Link>
+        </span>
         <span onClick={() => setIsShowMenu(false)}>About</span>
         <span onClick={() => setIsShowMenu(false)}>Contact</span>
-        <span onClick={() => setIsShowMenu(false)}>Write</span>
-        <span onClick={() => setIsShowMenu(false)}>Logout</span>
+        <span onClick={() => setIsShowMenu(false)}>
+          <Link className="link" to="/write">
+            Write
+          </Link>
+        </span>
+        {user && <span onClick={() => setIsShowMenu(false)}>Logout</span>}
       </nav>
       <div className="right">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0-8CYleGWDG2Vt9fKFSCnK7LPbp77RbUldw&usqp=CAU"
-          alt=""
-        />
+        {user ? (
+          <Link to="/settings">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0-8CYleGWDG2Vt9fKFSCnK7LPbp77RbUldw&usqp=CAU"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <div className="login-registar">
+            <span>
+              <Link className="link" to="/login">
+                login
+              </Link>
+            </span>
+            <span>
+              <Link className="link" to="/registar">
+                registar
+              </Link>
+            </span>
+          </div>
+        )}
+
         <FontAwesomeIcon className="icon" icon={faMagnifyingGlass} />
         <FontAwesomeIcon className="menu" icon={faBars} onClick={menuHandler} />
       </div>
